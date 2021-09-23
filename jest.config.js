5737f115ca6a7d03fe5c8ff53e2bc49db859656b
Bruno Oliveira <brunooliveira812@gmail.com>
2021-09-23 12:53:57 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  roots: [
+    "<rootDir>/src"
+  ],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
